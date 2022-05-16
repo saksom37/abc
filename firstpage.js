87366565b09bfc74a41530dd54be5d61bbfa7fc6
Saksham Walia <saksham1243.be21@chitkara.edu.in>
2022-05-16 19:54:31 +0530

    few edit in js file of index.html

--- firstpage.js
+++ firstpage.js
@@ -5,3 +5,14 @@ let secBox = document.getElementById("sec-box");
 let endDate = new Date(2023, 0, 1, 00, 00);
 let endTime = endDate.getTime();
 
+function countdown() {
+  let todayDate = new Date();
+  let todayTime = todayDate.getTime();
+  let remainingTime = endTime - todayTime;
+  let oneMin = 60 * 1000;
+  let oneHr = 60 * oneMin;
+  let oneDay = 24 * oneHr;
+
+ 
+}
+
