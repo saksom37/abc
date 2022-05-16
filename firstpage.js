50e22fcf0ba052894d81035054fd4a0979fe3769
Saksham Walia <saksham1243.be21@chitkara.edu.in>
2022-05-16 19:55:10 +0530

    final js file

--- firstpage.js
+++ firstpage.js
@@ -12,7 +12,27 @@ function countdown() {
   let oneMin = 60 * 1000;
   let oneHr = 60 * oneMin;
   let oneDay = 24 * oneHr;
+  let addZeroes = (num) => (num < 10 ? `0${num}` : num);
 
+  if (endTime < todayTime) {
+    clearInterval(i);
+    document.querySelector(
+      ".countdown"
+    ).innerHTML = `<h1>Countdown Has Expired</h1>`;
+  } else {
+    let daysLeft = Math.floor(remainingTime / oneDay);
+    let hrsLeft = Math.floor((remainingTime % oneDay) / oneHr);
+    let minsLeft = Math.floor((remainingTime % oneHr) / oneMin);
+    let secsLeft = Math.floor((remainingTime % oneMin) / 1000);
+  
+    dayBox.textContent = addZeroes(daysLeft);
+    hrBox.textContent = addZeroes(hrsLeft);
+    minBox.textContent = addZeroes(minsLeft);
+    secBox.textContent = addZeroes(secsLeft);
+  }
  
 }
 
+
+let i = setInterval(countdown, 1000);
+countdown();
